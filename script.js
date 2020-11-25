f9bb826d254f4a6345a4ca1e5148442a812b3c8c
SterlingDuke <sterlingcduke@gmail.com>
2020-11-25 09:31:12 -0600

    all changes

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 var eventBriteAPI = "OVMXD2IZ6NPP3KHS7G4E";
 var yelpAPI = "9VUtXRmjC3Psh50MTKDn-lpXsZIazhjlyx88TpX6WWLCHvk-_-DuGww3FkdhLhSDpIPlFvOzMjVSsJaS1hrIbG3FYNxznVAWr_UMGM4E8DbkOzrAz5pYOQY1qUW5X3Yx"
-// Initialize all input of date type.
-const calendars = bulmaCalendar.attach('[type="date"]', options);
+// Initialize all input of type date
+var calendars = bulmaCalendar.attach('[type="date"]', options);
 
 // Loop on each calendar initialized
-calendars.forEach(calendar => {
-// Add listener to date:selected event
-calendar.on('date:selected', date => {
-console.log(date);
-});
-});
+for(var i = 0; i < calendars.length; i++) {
+	// Add listener to select event
+	calendars[i].on('select', date => {
+		console.log(date);
+	});
+}
+
+// To access to bulmaCalendar instance of an element
+var element = document.querySelector('#my-element');
+if (element) {
+	// bulmaCalendar instance is available as element.bulmaCalendar
+	element.bulmaCalendar.on('select', function(datepicker) {
+		console.log(datepicker.data.value());
+	});
+}
