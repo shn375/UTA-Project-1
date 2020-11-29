98930ab7af3021c39a0022517c86692c782015d4
thamma <thammaraku@gmail.com>
2020-11-28 23:36:11 -0600

    clean up code

--- script.js
+++ script.js
@@ -9,32 +9,13 @@ var graceNoteMoviePrefix = "http://data.tmsapi.com/v1.1/movies/showings?";
 // var yelpApiPrefix = "https://api.yelp.com/v3/businesses/search?";
 // sample of request var yelpAPIPrefix = "https://api.yelp.com/v3/businesses/search?term=pizza&latitude=30.266666&longitude=-97.733330&apikey=kttxiZtG1Dr6SQS5GR-wLFn_d-Y4xjs0lqPoakVILmEO8UWcTXfrPZ9PV95L7-traBoAT0kEsK0vsOtq_r-CBmPLqxYYqHe-pKTh2xFNe5saVlDbTAtT0vUmiz25X3Yx";
 
-
-var omdbApiKey = "5673ffbb";
-var omdbPrefix = "http://www.omdbapi.com/?apikey="
-
-
 var genresSelected = "";
 var dateSelected = "";
 var zipCode = "";
-var radius = "";
-
-
-// Hardcode to verify API
 var genresSelected = "";
-// var dateSelected = "2020-11-27";
-// var zipCode = "78613";
 var zipCode = "";
-
 var radius = "5";
-
-// CURRENT DATE WORKING BUT TRY USER SELECTED DATE
-// var currentDate = moment().format('YYYY-MM-DD');
-// var currentDate = "";
-// console.log(currentDate);
-
-// var movieGenresMatch = [];
-var genresSearch = "Action";  // need to link this to event listener
+var genresSearch = "";
 var poster = "";
 
 
@@ -47,7 +28,6 @@ function getMovies(zipCode, genresSearch, dateTimeSearch) {
 
     // var currentDate = dateSearch;
 
-
     console.log("line 42 " + zipCode);
     console.log("line 42 " + genresSearch);
 
@@ -57,9 +37,7 @@ function getMovies(zipCode, genresSearch, dateTimeSearch) {
     // CURRENT DATE WORKING BUT TRY USER SELECTED SDA
     var movieUrlRequest = graceNoteMoviePrefix + "genres="+ genresSearch + "&startDate=" + dateTimeSearch + "&zip=" + zipCode + "&radius=" + radius + "&api_key=" + graceNoteMovieApi;
 
-
     console.log("line 58 " + movieUrlRequest);
-
 
     $.ajax({
         url: movieUrlRequest,
@@ -74,10 +52,7 @@ function getMovies(zipCode, genresSearch, dateTimeSearch) {
             // return addNewArray(response, genresSearch);
         })
 
-
 }
-
-
 
 //// 2 /////
 function addNewArray(response, zipCode, genresSearch) {
@@ -90,7 +65,6 @@ function addNewArray(response, zipCode, genresSearch) {
     console.log("line 80 " + response.length);
 
     var movieGenresMatch = [];
-
 
     for (i = 0; i < response.length; i++) {
         // for (i = 0; i < 3; i++) {
@@ -114,16 +88,8 @@ function addNewArray(response, zipCode, genresSearch) {
         var showtimesMatch = response[i].showtimes;
         var imageMatch = response[i].preferredImage.uri;
 
-
-       
-
-
-        // console.log("line 101 " + imageMatch);
-
         if (genresMatch.includes(genresSearch)) {
             console.log("line 109 loop " + i + "genresMatch " + genresMatch + "has " + genresSearch);
-
-            
 
             movieGenresMatch.push({ Title: movieMatch, Genres: genresSearch, Showtimes: showtimesMatch, Image: imageMatch });
 
@@ -132,7 +98,6 @@ function addNewArray(response, zipCode, genresSearch) {
             console.log("line 109 " + movieGenresMatch.length);
 
         }
-
 
         console.log("line 118 " + movieGenresMatch);
         console.log("line 118 " + movieGenresMatch.length);
@@ -150,9 +115,7 @@ function addNewArray(response, zipCode, genresSearch) {
     for (i = 0; i < movieGenresMatch.length; i++) {
         for (y = 0; y < movieGenresMatch[i].Showtimes.length; y++) {
 
-
             //  TRYING CONVERT 24H TO 12H
-
              var dateTimeBeforeConvert = movieGenresMatch[i].Showtimes[y].dateTime;
              console.log("line 155 dateTimeBeforeConvert " + dateTimeBeforeConvert);
              dateTimeAfterConvert = convertTime12H(dateTimeBeforeConvert);
@@ -193,10 +156,6 @@ function renderMoviesResult(newMovieArray) {
         var theatre = newMovieArray[i].Theatre;
         var imageUrl = newMovieArray[i].Image;
 
-        // console.log("line 201 " + imageUrl);
-        // console.log("line 191 " + movieName);
-        // console.log("line 192 " + theatre);
-
         if (movieName !== currentMovieName) {
             ++startCard
         }
@@ -204,43 +163,24 @@ function renderMoviesResult(newMovieArray) {
         var cardNumber = "mov-result-" + startCard;
         // same movie scenarios to display only one
         if (movieName === currentMovieName) {
-            // console.log("equal");
             cardNumber = currentCardNumber;
-            $(`#${cardNumber}`).find('.showTime').append("<p>" + newMovieArray[i].Theatre + "</p>");
+
+            // TRY MOVE UNDER THEATRE LOOP
             $(`#${cardNumber}`).find('.showTime').append("<p>" + newMovieArray[i].Showtimes + "</p>");
 
+            if (theatre === currentTheatre) {
+                console.log("line 215 " + "theatre= " + theatre + "currentTheatre= " + currentTheatre );
+            } else {
+                $(`#${cardNumber}`).find('.showTime').append("<p>" + newMovieArray[i].Theatre + "</p>");
+            }
+
         } else {
-            // console.log("not equal");
             $(`#${cardNumber}`).find('.mov-title').text(newMovieArray[i].Title);
             $(`#${cardNumber}`).find('.theatre').text(newMovieArray[i].Theatre);
             $(`#${cardNumber}`).find('.showTime').text(newMovieArray[i].Showtimes);
-            // ADDING MOVIE IMAGE WILL CHANGE TO POSTER
+
+            // FINALLY FOUND THE LINK TO POSTER
             $(`#${cardNumber}`).find('.movieThumb').attr('src', "http://demo.tmsimg.com/" + newMovieArray[i].Image);
-
-            // ++ startCard
-
-
-            // ///// TRY ADD MOVIE POSTER HERE
-
-            // var movieMatchNoSpace = newMovieArray[i].Title.replace(/ /g, "_");;
-            // var omdbUrlRequest = omdbPrefix + omdbApiKey + "&t=" + movieMatchNoSpace
-            // console.log("line 168 " + omdbUrlRequest);
-
-            // $.ajax({
-            //     url: omdbUrlRequest,
-            //     method: "GET",
-            // })
-            //     .then(function (response) {
-
-            //         poster = response.Poster
-            //         console.log("line 179 " + poster);
-
-            //         // $(`#${cardNumber}`).find('.movieThumb').attr('src', "Thammarak");
-
-            //     });
-
-
-
         }
 
         var currentMovieName = newMovieArray[i].Title;
@@ -251,104 +191,7 @@ function renderMoviesResult(newMovieArray) {
     // END OF FOR LOOP
 
 
-    // getPoster(newMovieArray);
 }
-
-
-//// 4 /////
-// function getPoster(newMovieArray) {
-
-
-//     console.log("line 155 " + newMovieArray);
-//     console.log("line 155 " + newMovieArray[0].Title);
-
-
-//     for (var i = 0; i < newMovieArray.length; i++) {
-//         // for (var i = 0; i < 1; i++) {
-//         // FOR DEBUG
-
-//         // var movieMatchNoSpace = newMovieArray[i].title.replace(/ /g, "_");
-//         var movieMatchNoSpace = newMovieArray[i].Title.replace(/ /g, "_");;
-//         console.log("line 165 " + movieMatchNoSpace);
-
-//         var omdbUrlRequest = omdbPrefix + omdbApiKey + "&t=" + movieMatchNoSpace
-//         console.log("line 168 " + omdbUrlRequest);
-//         $.ajax({
-//             url: omdbUrlRequest,
-//             method: "GET",
-//         })
-//             .then(function (response) {
-
-//                 addPosterToArray(response, newMovieArray);
-                
-//             });
-//     }
-
-// }
-
-//// 4 half /////
-
-// function addPosterToArray (response, newMovieArray) {
-
-
-
-//     for (i = 0; i < newMovieArray.length; i++) {
-//         var posterUrl = response.Poster;
-//         console.log("line 193 " + posterUrl);
-//         newMovieArray[i].Poster = posterUrl;
-
-//         console.log("line 197 newMovieArray[i].Poster " + newMovieArray[i].Poster);
-
-//     }
-
-
-//     renderPoster(newMovieArray);
-
-
-// }
-
-
-//// 5 /////
-// function renderPoster(newMovieArray) {
-
-
-//     var startCard = 0
-//     var currentMovieName = "";
-    
-//     for (var i = 0; i < newMovieArray.length; i++) {
-
-//         var movieName = newMovieArray[i].Title;
-
-
-//         if (movieName !== currentMovieName) {
-//             ++startCard
-//         }
-
-//         var cardNumber = "mov-result-" + startCard;
-//         // same movie scenarios to display only one
-//         if (movieName === currentMovieName) {
-//             console.log("equal movieName = currentMovieName " + movieName +'='+ currentMovieName);
-//             cardNumber = currentCardNumber;
-//             // $(`#${cardNumber}`).find('.showTime').append("<p>" + newMovieArray[i].Theatre + "</p>");
-//             // $(`#${cardNumber}`).find('.showTime').append("<p>" + newMovieArray[i].Showtimes + "</p>");
-
-//         } else {
-//             console.log("not equal movieName = currentMovieName " + movieName +'!='+ currentMovieName);
-//             console.log("line 217 " + newMovieArray[i].Poster);
-//             $(`#${cardNumber}`).find('.movieThumb').attr('src', newMovieArray[i].Poster);
-
-//             // ++ startCard
-
-//         }
-
-//         var currentMovieName = newMovieArray[i].Title;
-//         var currentCardNumber = cardNumber;
-
-
-//     }
-
-
-// }
 
 
 function convertTime12H(dateTimeBeforeConvert) {
@@ -371,36 +214,6 @@ function convertTime12H(dateTimeBeforeConvert) {
 //////////////////////////// EXECUTION ////////////////////////////////////////
 
 /////// 1 USER MUST SELECT ZIP AND GENRES FIRST BEFORE GET MOVIE RESULT ////////
-
-///// KEEP WORKING CODE START ////
-// alert("Please input your current location");
-
-// $("#location-button").click(function (event) {
-//     event.preventDefault;
-
-//     alert("Please select Movie Genres or Food Type")
-
-//     window.zipCode = $('input').val();
-//     console.log("line 270 " + zipCode);
-
-//     $(".target").change(function (event) {
-//         event.preventDefault;
-    
-//         window.genresSearch = $("#movie-selections option:selected").text();
-//         genresSearch = genresSearch.toLowerCase();
-//         console.log("line 270 " + genresSearch);
-    
-//         getMovies(zipCode, genresSearch);
-    
-//     });
-
-    
-// })
-///// KEEP WORKING CODE END ////
-
-
-///////////////////TRYING CODE ////////////////////////
-
 
 alert("Please input your current location");
 
@@ -434,3 +247,42 @@ $("#location-button").click(function (event) {
 
     
 })
+
+
+
+/////////////// BACKUP CODE OMDB /////////////////////
+
+
+//// 4 /////
+
+// var omdbApiKey = "5673ffbb";
+// var omdbPrefix = "http://www.omdbapi.com/?apikey="
+// function getPoster(newMovieArray) {
+
+
+//     console.log("line 155 " + newMovieArray);
+//     console.log("line 155 " + newMovieArray[0].Title);
+
+
+//     for (var i = 0; i < newMovieArray.length; i++) {
+//         // for (var i = 0; i < 1; i++) {
+//         // FOR DEBUG
+
+//         // var movieMatchNoSpace = newMovieArray[i].title.replace(/ /g, "_");
+//         var movieMatchNoSpace = newMovieArray[i].Title.replace(/ /g, "_");;
+//         console.log("line 165 " + movieMatchNoSpace);
+
+//         var omdbUrlRequest = omdbPrefix + omdbApiKey + "&t=" + movieMatchNoSpace
+//         console.log("line 168 " + omdbUrlRequest);
+//         $.ajax({
+//             url: omdbUrlRequest,
+//             method: "GET",
+//         })
+//             .then(function (response) {
+
+//                 addPosterToArray(response, newMovieArray);
+                
+//             });
+//     }
+
+// }
